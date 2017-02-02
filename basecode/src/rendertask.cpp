cfce570ca1d758649a9d8b7d1a08dc0998831c0a
HanmingZhang <HanmingZhang@hotmail.com>
2017-02-01 22:21:07 -0500

    hw2 second submit

--- basecode/src/rendertask.cpp
+++ basecode/src/rendertask.cpp
@@ -1,5 +1,9 @@
 #include <rendertask.h>
 
+const bool AAx2 = false;
+
+
+
 void RenderTask::run(){
 
     unsigned int upper_left_x = minPixel.x;
@@ -12,9 +16,26 @@ void RenderTask::run(){
     {
         for(unsigned int j = upper_left_y; j < lower_right_y; j++)
         {
-            Ray r = camera->Raycast((float)i, (float)j);
-            unsigned int depth = 0;
-            film->pixels[i][j] = integrator->TraceRay(r, depth);
+            if(AAx2){
+                Ray r1 = camera->Raycast((float)i + 0.25f, (float)j + 0.25f);
+                Ray r2 = camera->Raycast((float)i + 0.5f, (float)j + 0.25f);
+                Ray r3 = camera->Raycast((float)i + 0.25f, (float)j + 0.5f);
+                Ray r4 = camera->Raycast((float)i + 0.5f, (float)j + 0.5f);
+
+
+
+                unsigned int depth = 0;
+                film->pixels[i][j] = 0.25f * (integrator->TraceRay(r1, depth) +
+                                              integrator->TraceRay(r2, depth) +
+                                              integrator->TraceRay(r3, depth) +
+                                              integrator->TraceRay(r4, depth));
+            }
+
+            else{
+                 Ray r = camera->Raycast((float)i, (float)j);
+                 unsigned int depth = 0;
+                 film->pixels[i][j] = integrator->TraceRay(r, depth);
+            }
         }
     }
 }
